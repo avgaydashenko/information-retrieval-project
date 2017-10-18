e225302e7e169308a2e30fb964b52e9d0fa628d6
Anastasia Gaydashenko <a.v.gaydashenko@gmail.com>
2017-10-18 21:03:53 +0300

    robots.txt parser

---
+++ robots.py
@@ -0,0 +1,22 @@
+from reppy.robots import Robots
+from urllib.parse import urlsplit
+
+class Robot:
+    def __init__(self):
+        self.url_robot = {}
+
+    def get_domain(self, url):
+        return "{0.scheme}://{0.netloc}/".format(urlsplit(url))
+
+    def get_robots(self, url):
+        return self.get_domain(url) + 'robots.txt'
+
+    def is_allowed(self, url):
+        domain = self.get_domain(url)
+        if domain in self.url_robot:
+            return self.url_robot[domain].allowed(url)
+        else:
+            robot = Robots.fetch(self.get_robots(url))
+            agent = robot.agent('bot')
+            self.url_robot[domain] = agent
+            return agent.allowed(url)
