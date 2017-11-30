e9c7da298f6b57119c087332018debef1fc637e9
Anastasia Gaydashenko <a.v.gaydashenko@gmail.com>
2017-11-30 23:44:57 +0300

    example interface for query process function

---
+++ ranging.py
@@ -0,0 +1,40 @@
+class Ranging:
+    def __init__(self):
+        pass
+
+    def process_query(self, query):
+        return [{'title': 'Im Fadenkreuz von politischer Polizei und Geheimdiensten: Albert Einstein',
+                 'url': 'https://link.springer.com/chapter/10.1007%2F3-540-30595-5_7',
+                 'content': 'Im Fadenkreuz von politischer Polizei und Geheimdiensten: Albert Einstein'},
+                {'title': 'A Benchmarking Environment for Reinforcement Learning Based Task Oriented Dialogue Management',
+                 'url': 'https://arxiv.org/abs/1711.11023',
+                 'content': 'Dialogue assistants are rapidly becoming an indispensable daily aid. To avoid the significant effort need'},
+                {'title': 'Particle Optimization in Stochastic Gradient MCMC',
+                 'url': 'https://arxiv.org/abs/1711.10927',
+                 'content': 'Dialogue assistants are rapidly becoming an indispensable daily aid. To avoid the significant effort need'},
+                {'title': 'Introduction to Tensor Decompositions and their Applications in Machine Learning',
+                 'url': 'https://arxiv.org/abs/1711.10781',
+                 'content': 'Dialogue assistants are rapidly becoming an indispensable daily aid. To avoid the significant effort need'},
+                {'title': 'Dimension Reduction for Robust Covariate Shift Correction',
+                 'url': 'https://arxiv.org/abs/1711.10938',
+                 'content': 'Dialogue assistants are rapidly becoming an indispensable daily aid. To avoid the significant effort need'},
+                {'title': 'Deep Image Prior',
+                 'url': 'https://arxiv.org/abs/1711.10925',
+                 'content': 'Dialogue assistants are rapidly becoming an indispensable daily aid. To avoid the significant effort need'},
+                {'title': 'Semi-Supervised Few-Shot Learning with Prototypical Networks',
+                 'url': 'https://arxiv.org/abs/1711.10856',
+                 'content': 'Dialogue assistants are rapidly becoming an indispensable daily aid. To avoid the significant effort need'},
+                {'title': 'Tighter Lifting-Free Convex Relaxations for Quadratic Matching Problems',
+                 'url': 'https://arxiv.org/abs/1711.10733',
+                 'content': 'Dialogue assistants are rapidly becoming an indispensable daily aid. To avoid the significant effort need'},
+                {'title': 'Valid Inference Corrected for Outlier Removal',
+                 'url': 'https://arxiv.org/abs/1711.10635',
+                 'content': 'Dialogue assistants are rapidly becoming an indispensable daily aid. To avoid the significant effort need'},
+                {'title': 'TensorFlow Distributions',
+                 'url': 'https://arxiv.org/abs/1711.10604',
+                 'content': 'Dialogue assistants are rapidly becoming an indispensable daily aid. To avoid the significant effort need'}
+                ], [
+            {'title': 'Paper Title',
+             'url': 'https://link.springer.com/chapter/10.1007%2F3-540-30595-5_7',
+             'content': 'Authors: Albert Einstein, Some Other Guy'},
+        ]
